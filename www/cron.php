5eb7b380120c097aa45170fd216b51b198b46699
novichkovv <novichkovv@bk.ru>
2016-07-19 12:06:07 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/cron.php
+++ www/cron.php
@@ -2,5 +2,6 @@
 require_once('config.php');
 require_once(CORE_DIR . 'registry.php');
 require_once(CORE_DIR . 'autoload.php');
-$cron = new cron_class();
-$cron->upwork();
+tools_class::mail('Upwork Feed', 'asss', 'novichkovv@bk.ru');
+//$cron = new cron_class();
+//$cron->upwork();
