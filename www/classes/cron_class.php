bd13396d39da40f6685945b8df5a8b4af0bf0e60
novichkovv <novichkovv@bk.ru>
2017-03-16 00:00:04 +0700

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/classes/cron_class.php
+++ www/classes/cron_class.php
@@ -9,6 +9,7 @@ class cron_class extends base
         }
         $res = '';
         $rows = [];
+        echo 7;
         $text = file_get_contents('https://www.upwork.com/ab/feed/topics/rss?securityToken=f5b4a3e2c9db58a24f985e078b7b90f2b80a98ce4f982d9a371757afcbd2fbb27e6e7a7b2276eb666f63c230509e66f6bb8fb771e86444e62da3decb785cc5e3&userUid=540085637412036608');
         $xml = new SimpleXMLElement($text);
         foreach ($xml->channel->item as $item) {
